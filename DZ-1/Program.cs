c0da5c744b4a0deae6f6c526566ead802f150c5b
Alexey <qoqo.s@yandex.ru>
2022-02-17 10:42:27 +0300

    Update Program.cs

--- DZ-1/Program.cs
+++ DZ-1/Program.cs
@@ -4,9 +4,49 @@ namespace DZ1
 {
     class MainClass
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many nails? ");
+            int nails = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            //while (0 >= nails || nails > 10)
+            //{
+            //    Console.WriteLine("Dude.. Wrong input data! We need nails > 0 and <= 10");
+            //}
+
+            if (0 < nails && nails <= 10)
+            {
+                Console.WriteLine("How many hits?");
+                int hits = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (0 < hits && hits <= 5)
+                {
+                    for (int i = 1; i != nails + 1; i++)
+                    {
+                        Console.WriteLine("Nail #" + i);
+                        var n = hits;
+                        while (n > 0)
+                        {
+                            Console.WriteLine("Knock!");
+                            n--;
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Dude.. Wrong input data! We need hits > 0 and <= 5");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dude.. Wrong input data! We need nails > 0 and <= 10");
+
+            }
         }
     }
 }
+
